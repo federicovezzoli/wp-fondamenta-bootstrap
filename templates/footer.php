c061bf810c31903f5d4383e176f52c0d7c0cf48f
Federico Vezzoli <federico@dot-agency.it>
2018-03-12 17:32:10 +0100

    updated bootstrap classes

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,10 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
   	<div class="row">
-  		<div class="col-xs-12 col-sm-9">
+  		<div class="col-12 col-sm-9">
   			<?php dynamic_sidebar('sidebar-footer'); ?>
   		</div>
-  		<div class="col-xs-12 col-sm-3">
+  		<div class="col-12 col-sm-3">
   			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
   		</div>
   	</div>
